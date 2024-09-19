5f9130a543c8be1900974b1000168ccb4b59e09c
Chris Lott <rclott@hiwaay.net>
2024-09-20 07:56:21 +0900

    experiments with formats, python output, byte option, pyproject.toml

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # rgb565-converter
 
+*This is TheStumbler fork, see below*
+
 [![CI](https://github.com/CommanderRedYT/rgb565-converter/actions/workflows/ci.yaml/badge.svg)](https://github.com/CommanderRedYT/rgb565-converter/actions/workflows/ci.yaml)
 
 A simple script to help converting png files to rgb565 (used in [TFT_eSPI](https://github.com/Bodmer/TFT_eSPI))
+
 
 ## Install
 ```bash
@@ -11,3 +14,27 @@ pip install rgb565-converter
 # or
 pip3 install rgb565-converter
 ```
+
+## The Stumbler's Branch
+
+While doing some experiments displaying images on TFT using MicroPython,
+I found this rgb565 converter. It didn't exactly do what I needed, so I
+started changing it.
+
+* Added byte vs word output options
+* Option to generate Python output vs C++
+* Added crude handling of RGB/RGBA and 4-bit grayscale. 
+* Changed the way output hex was built, string-wise to list-wise
+* Completely ignored the reverse direction, binary data back to PNG
+* added `pyproject.toml` b/c `setup.py` and `pip install -e` *deprecation*
+
+These changes are very crude and not suitable for a pull request. They 
+are primarily for learning and experimenting. Some (all?) of the
+manipulations here could probably be done using Imagemagick to tweak
+the PNG before even calling this program.
+
+Example of running the program to generate Python output
+
+```(.venv) $ rgb565-converter -b1 -i bb.png -o bb.py```
+
+
--- rgb565_converter/converter.py
+++ rgb565_converter/converter.py
@@ -5,6 +5,7 @@ from PIL import Image
 from enum import Enum
 
 class Mode(Enum):
+    PY = ".py"
     CPP = ".cpp"
     PNG = ".png"
 
@@ -32,6 +33,12 @@ def main():
         dest="swap",
         help="Swap bytes for 16-bit words."
     )
+    parser.add_argument(
+        "-b",
+        "--bytes",
+        dest="bytes",
+        help="Saves as bytes instead of 16-bit words."
+    )
     args = parser.parse_args()
 
     input_basename = os.path.basename(args.input_file).rsplit('.', 1)
@@ -51,7 +58,7 @@ def main():
         print("Error: Input file must be a .png or .cpp file.")
         exit(1)
 
-    if (output_basename[1] not in ['png', 'cpp']):
+    if (output_basename[1] not in ['png', 'cpp', 'py']):
         print("Error: Output file must be a .png or .cpp file.")
         print(f"Output file: {output_basename}")
         exit(1)
@@ -66,28 +73,106 @@ def main():
         convert_png_to_rgb565(args)
 
 def convert_png_to_rgb565(args):
-    name = os.path.basename(args.output_file).rsplit('.', 1)[0]
+    output_basename = os.path.basename(args.output_file).rsplit('.', 1)
+    # name = output_basename[0]
+    name = 'icon'
     png = Image.open(args.input_file)
     width, height = png.size
 
-    max_line_width = min(width, 64)
+    if   output_basename[1] == 'png': omode = Mode.PNG
+    elif output_basename[1] == 'cpp': omode = Mode.CPP 
+    elif output_basename[1] == 'py':  omode = Mode.PY 
+
+    if   omode == Mode.PNG: print("Out mode: PNG")
+    elif omode == Mode.CPP: print("Out mode: CPP")
+    elif omode == Mode.PY:  print("Out mode: PY")
+
+    max_line_width = min(width, 72)
 
     # iterate over the pixels
     image = png.getdata()
-    image_content = ""
-    for i, pixel in enumerate(image):
+    color = png.mode
+    # MicroPython pixel format constants from the framebuf class
+    # framebuf.RGB565 = 1
+    # framebuf.GS4_HMSB = 2
+    # for RGBA, Hackaday Wrencher logo, 
+    # has almost all zeros as RGB and 
+    # all the information is in the A channel
+    # convert it to amber color...
+    amber =	(255.0/255.0, 192.0/255.0, 0)
+
+    print(f'image format: {color}' )
+
+    if 'RGB' in color: # this is color image
+      packing = 1
+      rgb8 = [0,0]
+      pixels = []
+      for i, pix in enumerate(image):
+        if len(pix) == 4:
+          pixel=[0,0,0]
+          for k in range(3):
+            pixel[k] = int( pix[3] * amber[k] )
+        else:
+          pixel = pix
+        # print(i, pixel)
         r = (pixel[0] >> 3) & 0x1F
         g = (pixel[1] >> 2) & 0x3F
         b = (pixel[2] >> 3) & 0x1F
         rgb = r << 11 | g << 5 | b
-
         if args.swap:
-            rgb = ((rgb & 0xFF) << 8) | ((rgb & 0xFF00) >> 8)
-        
-        image_content += f"0x{rgb:04X}" + (",\n    " if (i % max_line_width == max_line_width-1) else ",")
+          rgb = ((rgb & 0xFF) << 8) | ((rgb & 0xFF00) >> 8)
+        rgb8[0] = (rgb & 0xff00) >> 8
+        rgb8[1] = (rgb & 0xff)
+        pixels.extend( rgb8 )
 
-    if image_content.endswith("\n    "):
-        image_content = image_content[:-5]
+    elif 'L' in color: # this is grayscale image
+      packing = 2
+      # hist = png.histogram()
+      # for i, num in enumerate(hist):
+      #   if num: 
+      #     print(i, num)
+      # get max / min
+      gs_max = 0
+      gs_min = 256
+      for i, pix in enumerate(image):
+        if type(pix) is int:  pixel = pix
+        else:                 pixel = pix[1]
+        if pixel > gs_max: gs_max = pixel
+        elif pixel < gs_min: gs_min = pixel
+        #print(f'{i:8}{pixel:8}{gs_min:8}{gs_max:8}')
+      print(f'min/max: {gs_min}  {gs_max}')
+      pixels = []
+      gs = [] # temporary holding buffer
+      for i, pix in enumerate(image):
+        # get the pixel value
+        if type(pix) is int:  pixel = pix
+        else:                 pixel = pix[1]
+        # scale and store in temporary nibble buffer
+        gs.append( int(pixel/17) )
+        if len(gs) >= 2:
+          # build combined nibble
+          pixels.append( ((gs[0] & 0x0f)<<4) + (gs[1] & 0x0f) )
+          # reset
+          gs = [] 
+      # wrap up straggling pixel(s)
+      if len(gs) == 1:
+        pixels.append( (gs[0] & 0x0f)<<4 )
+
+    # now print it
+    image_content = "  "
+    nominal_line_width = 64
+    icol = 0
+    for pixel in pixels:
+      hexbyte = f'0x{pixel:02x},' 
+      icol += len(hexbyte)
+      image_content += hexbyte
+      if icol > nominal_line_width:
+        image_content += '\n    ' 
+        icol = 0
+
+    # wrap up, remove trailing newline if necessary
+    if image_content.endswith('\n    '):
+      image_content = image_content[:-5]
 
     output_h_content = f"""
 #pragma once
@@ -110,12 +195,34 @@ const espgui::Icon<{width}, {height}> {name}{{{{
 }} // namespace icons
     """.strip() + "\n"
 
-    with open(args.output_file, 'w') as output_file:
-        output_file.write(output_cpp_content)
+    output_python_class = f"""
+class Image:
+  def __init__( self, _wid, _hgt, _packing, _buf):
+    self.wid = _wid
+    self.hgt = _hgt
+    self.buf = _buf
+    self.packing = _packing
+    """.strip() + "\n\n\n"
 
-    with open(args.output_file.replace('.cpp', '.h'), 'w') as output_file:
-        output_file.write(output_h_content)
+    output_python_content = f"""
+# image format: {color}
+{name} = Image( {width}, {height}, {packing},
+  bytearray( [
+  {image_content}
+  ] )
+)
+    """.strip() + "\n"
 
+    if omode == Mode.CPP:
+      with open(args.output_file, 'w') as output_file:
+          output_file.write(output_cpp_content)
+      with open(args.output_file.replace('.cpp', '.h'), 'w') as output_file:
+          output_file.write(output_h_content)
+
+    if omode == Mode.PY:
+      with open(args.output_file, 'w') as output_file:
+          output_file.write(output_python_class)
+          output_file.write(output_python_content)
 
 def convert_rgb565_to_png(args):
     with open(args.input_file, 'r') as input_file:
